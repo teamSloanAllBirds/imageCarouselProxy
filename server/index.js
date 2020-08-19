846ea908be10e3d1b8dc141cdaa38abe902d90c1
Owen Arthur <owenarthurdesign@gmail.com>
2020-08-18 21:25:20 -0400

    all components rendering with early mostly accurate styling

--- server/index.js
+++ server/index.js
@@ -12,8 +12,13 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/api/images/:product_id', createProxyMiddleware({ target: 'http://localhost:5000/', changeOrigin: true }));
+app.use('/api/midpageimages/:id', createProxyMiddleware({ target: 'http://localhost:7000/', changeOrigin: true }));
 app.use('/api/productreviews/reviews', createProxyMiddleware({ target: 'http://localhost:4000/', changeOrigin: true }));
 app.use('/api/productoptions/:id', createProxyMiddleware({ target: 'http://localhost:3001/', changeOrigin: true }));
+
+app.get('/:id', (req, res) => {
+  res.sendFile('index.html', {root: path.join(__dirname, '../public')});
+});
 
 app.listen(port, ()=>{
   console.log(`Server is running at http://127.0.0.1:${port}`)
